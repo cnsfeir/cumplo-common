371dbb0a8f9a94402fa03aaab552babffce9b8d1
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-12 21:22:32 -0400

    ✨ Add API key attribute to User model

--- cumplo_common/models/user.py
+++ cumplo_common/models/user.py
@@ -9,6 +9,7 @@ from cumplo_common.models.notification import Notification
 class User(BaseModel):
     id: str = Field(...)
     name: str = Field(...)
+    api_key: str = Field(...)
     is_admin: bool = Field(False)
     webhook_url: str | None = Field(None)
     notifications: dict[int, Notification] = Field(default_factory=dict, exclude=True)
