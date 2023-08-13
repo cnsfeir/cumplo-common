96658796912ffaa3f275c79d18601eaf44a4b02b
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-13 17:08:25 -0400

    ✨ Add max_notification attribute to User model

--- cumplo_common/models/user.py
+++ cumplo_common/models/user.py
@@ -11,6 +11,7 @@ class User(BaseModel):
     name: str = Field(...)
     api_key: str = Field(...)
     is_admin: bool = Field(False)
+    max_notifications: int = Field(1)
     webhook_url: str | None = Field(None)
     notifications: dict[int, Notification] = Field(default_factory=dict, exclude=True)
     configurations: dict[int, Configuration] = Field(default_factory=dict, exclude=True)
