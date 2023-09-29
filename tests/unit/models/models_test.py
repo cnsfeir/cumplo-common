8c2cd08ae287a8a588e7ad8606de7b8c571f3d7f
Cristobal Sfeir <cnsfeir@uc.cl>
2023-09-28 21:23:11 -0300

    ✅ Add unit tests for the custom StrEnum

---
+++ tests/unit/models/models_test.py
@@ -0,0 +1,23 @@
+from cumplo_common.models import StrEnum
+
+
+class TestStrEnum:
+    class HTTPVerb(StrEnum):
+        GET = "GET"
+        POST = "post"
+        PATCH = "Patch"
+
+    def test_in_statement(self) -> None:
+        """Should be able to use `in` statement with StrEnum members case-insensitively"""
+        for verb in ("GET", "POST", "PATCH"):
+            for verb_case in (verb.upper(), verb.lower(), verb.title()):
+                assert self.HTTPVerb.has_member(verb_case)
+        for verb in ("RUN", "JUMP", "SWIM", "FLY"):
+            for verb_case in (verb.upper(), verb.lower(), verb.title()):
+                assert not self.HTTPVerb.has_member(verb_case)
+
+    def test_instance(self) -> None:
+        """Should be able to create instances of StrEnum members case-insensitively"""
+        for verb in ("GET", "POST", "PATCH"):
+            for verb_case in (verb.upper(), verb.lower(), verb.title()):
+                assert self.HTTPVerb(verb_case) == getattr(self.HTTPVerb, verb.upper())
