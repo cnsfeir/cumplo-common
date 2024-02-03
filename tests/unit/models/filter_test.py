5e2f079b3e8d4b39b46aec028bed70566892a7ae
Cristobal Sfeir <cnsfeir@uc.cl>
2024-02-03 17:22:14 -0300

    ✅ Update unit tests

--- tests/unit/models/filter_test.py
+++ tests/unit/models/filter_test.py
@@ -1,37 +1,52 @@
 from pydantic import TypeAdapter
 
-from cumplo_common.models.filter import FilterConfiguration
+from cumplo_common.models.filter_configuration import FilterConfiguration
 
 
 class TestFilterConfiguration:
-    def test_equal_empty(self) -> None:
-        """Should be equal if two empty filters are compared"""
-        filter_1 = TypeAdapter(FilterConfiguration).validate_python({"id": 1})
-        filter_2 = TypeAdapter(FilterConfiguration).validate_python({"id": 2})
+    id_1 = "01HNQV9YFK3JZW69Z0CT3QK1QB"
+    id_2 = "01HNQVAV9GFV5NB80ZEVV9AWC1"
+
+    @classmethod
+    def test_equal_empty(cls) -> None:
+        """
+        Should be equal if two empty filters are compared
+        """
+        filter_1 = TypeAdapter(FilterConfiguration).validate_python({"id": cls.id_1})
+        filter_2 = TypeAdapter(FilterConfiguration).validate_python({"id": cls.id_2})
         assert filter_1 == filter_2
 
-    def test_equal_ignored_attributes(self) -> None:
-        """Should ignore the ID, name and expiration_minutes when comparing two filters"""
-        filter_1 = TypeAdapter(FilterConfiguration).validate_python({"id": 1, "name": "Name"})
-        filter_2 = TypeAdapter(FilterConfiguration).validate_python({"id": 2, "name": "Name"})
+    @classmethod
+    def test_equal_ignored_attributes(cls) -> None:
+        """
+        Should ignore the ID, name and expiration_minutes when comparing two filters
+        """
+        filter_1 = TypeAdapter(FilterConfiguration).validate_python({"id": cls.id_1, "name": "Name"})
+        filter_2 = TypeAdapter(FilterConfiguration).validate_python({"id": cls.id_2, "name": "Name"})
         assert filter_1 == filter_2
 
-    def test_equal_same_attributes(self) -> None:
-        """Should be equal if the key attributes are the same"""
+    @classmethod
+    def test_equal_same_attributes(cls) -> None:
+        """
+        Should be equal if the key attributes are the same
+        """
         filter_1 = TypeAdapter(FilterConfiguration).validate_python(
-            {"id": 1, "filter_dicom": True, "irr": "1.1", "score": 0.5, "duration": 30}
+            {"id": cls.id_1, "ignore_dicom": True, "minimum_irr": "1.1", "minimum_score": 0.5, "minimum_duration": 30}
         )
         filter_2 = TypeAdapter(FilterConfiguration).validate_python(
-            {"id": 2, "filter_dicom": True, "irr": "1.1", "score": 0.5, "duration": 30}
+            {"id": cls.id_2, "ignore_dicom": True, "minimum_irr": "1.1", "minimum_score": 0.5, "minimum_duration": 30}
         )
         assert filter_1 == filter_2
 
-    def test_different_attributes(self) -> None:
-        """Should NOT be equal if one of the key attributes are the different"""
+    @classmethod
+    def test_different_attributes(cls) -> None:
+        """
+        Should NOT be equal if one of the key attributes are the different
+        """
         filter_1 = TypeAdapter(FilterConfiguration).validate_python(
-            {"id": 1, "filter_dicom": True, "irr": "1.1", "score": 0.5, "duration": 30}
+            {"id": cls.id_1, "ignore_dicom": True, "minimum_irr": "1.1", "minimum_score": 0.5, "minimum_duration": 30}
         )
         filter_2 = TypeAdapter(FilterConfiguration).validate_python(
-            {"id": 2, "filter_dicom": True, "irr": "1.1", "score": 0.5, "duration": 10}
+            {"id": cls.id_2, "ignore_dicom": True, "minimum_irr": "1.1", "minimum_score": 0.5, "minimum_duration": 10}
         )
         assert filter_1 != filter_2
--- tests/unit/models/notification_test.py
+++ tests/unit/models/notification_test.py
@@ -9,22 +9,28 @@ class TestNotification:
     base_notification = {"date": "2021-01-01T00:00:00+00:00"}
 
     def test_invalid_id_format(self) -> None:
-        """Should raise a ValueError if the ID format is invalid"""
+        """
+        Should raise a ValueError if the ID format is invalid
+        """
         invalid_ids = ("123_alpha", "alpha-beta_123", "alpha_", "_123", "alpha123")
         for id_ in invalid_ids:
             with raises(ValueError) as error:
                 TypeAdapter(Notification).validate_python({"id": id_, **self.base_notification})
-            assert str(error.value) == "Invalid ID format"
+            assert "Invalid ID format" in str(error.value)
 
     def test_invalid_id_values(self) -> None:
-        """Should raise a ValueError if the ID value is invalid"""
+        """
+        Should raise a ValueError if the ID value is invalid
+        """
         invalid_ids = ("alpha_123", "BETA_456", "MixEDCaSe_789")
         for id_ in invalid_ids:
             with raises(ValueError):
                 TypeAdapter(Notification).validate_python({"id": id_, **self.base_notification})
 
     def test_valid_id(self) -> None:
-        """Should process the ID correctly"""
+        """
+        Should process the ID correctly
+        """
         invalid_ids = ("PROMISING_123", "Promising_456", "promising_789")
         for id_ in invalid_ids:
             notification = TypeAdapter(Notification).validate_python({"id": id_, **self.base_notification})
@@ -32,7 +38,9 @@ class TestNotification:
             assert notification.content_id == int(id_.split("_")[1])
 
     def test_build_id(self) -> None:
-        """Should build the ID correctly"""
+        """
+        Should build the ID correctly
+        """
         for template in Template.members():
             id_ = Notification.build_id(template, 1001)
             assert id_ == f"{template.value}_1001"
