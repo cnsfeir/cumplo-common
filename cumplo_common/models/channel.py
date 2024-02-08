cefda15729ae8a2824985ea8af45c81ed8191e1f
Cristobal Sfeir <cnsfeir@uc.cl>
2024-02-07 21:03:00 -0300

    ♻️ Simplify setting the channel type

--- cumplo_common/models/channel.py
+++ cumplo_common/models/channel.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from pydantic import Field
 
 from cumplo_common.models.base_model import BaseModel, StrEnum
@@ -17,16 +15,12 @@ class ChannelConfiguration(BaseModel):
 
 class WebhookConfiguration(ChannelConfiguration):
     url: str = Field(...)
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(type_=ChannelType.WEBHOOK, *args, **kwargs)
+    type_: ChannelType = ChannelType.WEBHOOK
 
 
 class IFTTTConfiguration(ChannelConfiguration):
     key: str = Field(...)
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(type_=ChannelType.IFTTT, *args, **kwargs)
+    type_: ChannelType = ChannelType.IFTTT
 
 
 CHANNEL_CONFIGURATION_BY_TYPE: dict[ChannelType, type[ChannelConfiguration]] = {
