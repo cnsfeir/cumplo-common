f8aa5cced59c36b0644f6e695e0f4dc206c0377b
Cristobal Sfeir <cnsfeir@uc.cl>
2024-02-13 21:28:14 -0300

    🔊 Add debugging logs

--- cumplo_common/dependencies/authentication.py
+++ cumplo_common/dependencies/authentication.py
@@ -1,6 +1,7 @@
 # pylint: disable=raise-missing-from
 
 from http import HTTPStatus
+from logging import getLogger
 from typing import Annotated
 
 from fastapi import Header
@@ -8,6 +9,8 @@ from fastapi.exceptions import HTTPException
 from fastapi.requests import Request
 
 from cumplo_common.database import firestore
+
+logger = getLogger(__name__)
 
 
 async def authenticate(request: Request, x_api_key: Annotated[str | None, Header()] = None) -> None:
@@ -25,15 +28,18 @@ async def authenticate(request: Request, x_api_key: Annotated[str | None, Header
         try:
             user = firestore.client.users.get(api_key=x_api_key)
         except (KeyError, ValueError):
+            logger.debug("Received invalid API key")
             raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
 
     elif event := getattr(request.state, "event", None):
         try:
             user = firestore.client.users.get(id_user=event.id_user)
         except (KeyError, ValueError):
+            logger.debug("Received invalid user ID")
             raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
 
     else:
+        logger.debug("No authentication method provided")
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
 
     request.state.user = user
