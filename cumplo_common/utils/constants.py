07a7c7b6aacf23398690a2cc1234652591533578
Cristobal Sfeir <cnsfeir@uc.cl>
2024-02-28 19:59:04 -0300

    ♻️ Reorder constants

--- cumplo_common/utils/constants.py
+++ cumplo_common/utils/constants.py
@@ -1,17 +1,25 @@
-from os import getenv
+import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-CHANNELS_COLLECTION: str = getenv("CHANNELS_COLLECTION", "channels")
-CUMPLO_BASE_URL: str = getenv("CUMPLO_BASE_URL", "")
-DEFAULT_EXPIRATION_MINUTES: int = int(getenv("DEFAULT_EXPIRATION_MINUTES", "60"))
-FILTERS_COLLECTION: str = getenv("FILTERS_COLLECTION", "filters")
-LOCATION: str = getenv("LOCATION", "us-central1")
-NOTIFICATIONS_COLLECTION: str = getenv("NOTIFICATIONS_COLLECTION", "notifications")
-PROJECT_ID: str = getenv("PROJECT_ID", "")
-SERVICE_ACCOUNT_EMAIL: str = getenv("SERVICE_ACCOUNT_EMAIL", "")
-USERS_COLLECTION: str = getenv("USERS_COLLECTION", "users")
-DICOM_STRINGS: list[str] = ["CON DICOM", "CONDICOM", "PRESENTA DICOM"]
-SIMULATION_AMOUNT = int(getenv("SIMULATION_AMOUNT", "1000000"))
+# Basics
+LOCATION = os.getenv("LOCATION", "us-central1")
+PROJECT_ID = os.getenv("PROJECT_ID", "")
+LOG_FORMAT = "\n%(levelname)s: %(message)s"
+IS_TESTING = bool(os.getenv("IS_TESTING"))
+SERVICE_ACCOUNT_EMAIL: str = os.getenv("SERVICE_ACCOUNT_EMAIL", "")
+
+# Firestore Collections
+CHANNELS_COLLECTION: str = os.getenv("CHANNELS_COLLECTION", "channels")
+FILTERS_COLLECTION: str = os.getenv("FILTERS_COLLECTION", "filters")
+NOTIFICATIONS_COLLECTION: str = os.getenv("NOTIFICATIONS_COLLECTION", "notifications")
+USERS_COLLECTION: str = os.getenv("USERS_COLLECTION", "users")
+
+# Cumplo
+CUMPLO_BASE_URL: str = os.getenv("CUMPLO_BASE_URL", "")
+SIMULATION_AMOUNT = int(os.getenv("SIMULATION_AMOUNT", "1000000"))
+
+# Defaults
+DEFAULT_EXPIRATION_MINUTES: int = int(os.getenv("DEFAULT_EXPIRATION_MINUTES", "60"))
