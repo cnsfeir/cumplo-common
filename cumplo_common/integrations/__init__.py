58eea392fb5ae12a450a91e0994f34a095c825af
Cristóbal Sfeir <cnsfeir@uc.cl>
2024-10-26 20:37:11 -0300

    ♻️ Fix internal import system (again)

---
+++ cumplo_common/integrations/__init__.py
@@ -0,0 +1,2 @@
+from .cloud_pubsub import CloudPubSub
+from .cloud_tasks import CloudTasks
--- cumplo_common/integrations/cloud_tasks.py
+++ cumplo_common/integrations/cloud_tasks.py
@@ -10,64 +10,68 @@ from google.protobuf.timestamp_pb2 import Timestamp
 from cumplo_common.utils.constants import LOCATION, PROJECT_ID, SERVICE_ACCOUNT_EMAIL
 
 
-def create_http_task(  # noqa: PLR0913, PLR0917
-    url: str,
-    queue: str,
-    payload: dict,
-    task_id: str,
-    headers: dict[str, str] | None = None,
-    dispatch_deadline: int | None = None,
-    schedule_time: datetime | None = None,
-    http_method: Annotated[int, HttpMethod] = HttpMethod.POST,
-    *,
-    is_internal: bool = True,
-) -> Task:
-    """
-    Create an HTTP POST task with a JSON payload.
+class CloudTasks:
+    """Integration with Google Cloud Tasks."""
 
-    Args:
-        url (str): Destination URL
-        queue (str): Queue name
-        payload (dict): Request JSON payload
-        headers (dict[str, str], optional): Request headers. Defaults to None.
-        task_id (str): Task identifier
-        dispatch_deadline (int | None, optional): Seconds until the task is dispatched. Defaults to None.
-        schedule_time (datetime | None, optional): Time at which the task will be scheduled. Defaults to None.
-        http_method (Annotated[int, HttpMethod], optional): HTTP method to use. Defaults to HttpMethod.POST.
-        is_internal (bool, optional): Whether the task is intended for internal use. Defaults to True.
+    @staticmethod
+    def create_http_task(  # noqa: PLR0913, PLR0917
+        url: str,
+        queue: str,
+        payload: dict,
+        task_id: str,
+        headers: dict[str, str] | None = None,
+        dispatch_deadline: int | None = None,
+        schedule_time: datetime | None = None,
+        http_method: Annotated[int, HttpMethod] = HttpMethod.POST,
+        *,
+        is_internal: bool = True,
+    ) -> Task:
+        """
+        Create an HTTP POST task with a JSON payload.
 
-    Returns:
-        Task: A unit of scheduled work
+        Args:
+            url (str): Destination URL
+            queue (str): Queue name
+            payload (dict): Request JSON payload
+            headers (dict[str, str], optional): Request headers. Defaults to None.
+            task_id (str): Task identifier
+            dispatch_deadline (int | None, optional): Seconds until the task is dispatched. Defaults to None.
+            schedule_time (datetime | None, optional): Time at which the task will be scheduled. Defaults to None.
+            http_method (Annotated[int, HttpMethod], optional): HTTP method to use. Defaults to HttpMethod.POST.
+            is_internal (bool, optional): Whether the task is intended for internal use. Defaults to True.
 
-    """
-    client = CloudTasksClient()
+        Returns:
+            Task: A unit of scheduled work
 
-    headers = headers or {}
-    task_id = f"{task_id}-{str(arrow.utcnow().timestamp()).replace('.', '')}"
-    name = client.task_path(PROJECT_ID, LOCATION, queue, task_id)
+        """
+        client = CloudTasksClient()
 
-    http_request = HttpRequest(
-        url=url,
-        http_method=http_method,
-        body=json.dumps(payload).encode(),
-        headers={**headers, "Content-type": "application/json"},
-    )
+        headers = headers or {}
+        task_id = f"{task_id}-{str(arrow.utcnow().timestamp()).replace('.', '')}"
+        name = client.task_path(PROJECT_ID, LOCATION, queue, task_id)
 
-    if is_internal:
-        http_request.oidc_token = OidcToken(service_account_email=SERVICE_ACCOUNT_EMAIL)
+        http_request = HttpRequest(
+            url=url,
+            http_method=http_method,
+            body=json.dumps(payload).encode(),
+            headers={**headers, "Content-type": "application/json"},
+        )
 
-    task = Task(name=name, http_request=http_request)
+        if is_internal:
+            http_request.oidc_token = OidcToken(service_account_email=SERVICE_ACCOUNT_EMAIL)
 
-    if schedule_time is not None:
-        timestamp = Timestamp()
-        timestamp.FromDatetime(schedule_time)
-        task.schedule_time = timestamp
+        task = Task(name=name, http_request=http_request)
 
-    if dispatch_deadline is not None:
-        duration = Duration()
-        duration.FromSeconds(dispatch_deadline)
-        task.dispatch_deadline = duration
+        if schedule_time is not None:
+            timestamp = Timestamp()
+            timestamp.FromDatetime(schedule_time)
+            task.schedule_time = timestamp
 
-    parent = client.queue_path(project=PROJECT_ID, location=LOCATION, queue=queue)
-    task_request = CreateTaskRequest(parent=parent, task=task)
-    return client.create_task(request=task_request)
+        if dispatch_deadline is not None:
+            duration = Duration()
+            duration.FromSeconds(dispatch_deadline)
+            task.dispatch_deadline = duration
+
+        parent = client.queue_path(project=PROJECT_ID, location=LOCATION, queue=queue)
+        task_request = CreateTaskRequest(parent=parent, task=task)
+        return client.create_task(request=task_request)
