e77b58d71de8a3f50aa2a99c3bc384471be5f798
Cristóbal Sfeir <cnsfeir@uc.cl>
2025-04-22 20:03:46 -0400

    feat: improve gmail integration

--- cumplo_common/integrations/gmail.py
+++ cumplo_common/integrations/gmail.py
@@ -1,15 +1,20 @@
 import base64
+import re
+from collections import UserDict
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from logging import getLogger
+from operator import itemgetter
 from pathlib import Path
-from typing import ClassVar
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from pydantic import BaseModel
 
-from cumplo_common.utils.constants import GMAIL_CREDENTIALS, GMAIL_FROM_EMAIL, GMAIL_USER_ID
+from cumplo_common.utils.constants import GMAIL_CREDENTIALS, GMAIL_FROM_EMAIL, GMAIL_LABEL, GMAIL_TOPIC, GMAIL_USER_ID
+
+logger = getLogger(__name__)
 
 
 class Attachment(BaseModel):
@@ -19,25 +24,77 @@ class Attachment(BaseModel):
     content_id: str
 
 
+class Message(UserDict):
+    """A Gmail message."""
+
+    @property
+    def sender(self) -> str | None:
+        """The sender of the message."""
+        headers = self.data.get("payload", {}).get("headers", [])
+        sender = next((header["value"] for header in headers if header["name"] == "From"), None)
+
+        if not sender:
+            logger.error(f"No sender found in message: {self.data}")
+            return None
+
+        if not (result := re.search(r"<([^>]+)>", sender)):
+            logger.error(f"No email found in {sender=}")
+            return None
+
+        return result.group(1)
+
+
 class Gmail:
     """Integration with Gmail API."""
-
-    SCOPES: ClassVar[list[str]] = ["https://mail.google.com/"]
 
     @classmethod
     def _authenticate(cls) -> build:
         """Authenticate with Gmail API."""
-        credentials = service_account.Credentials.from_service_account_file(
-            filename=GMAIL_CREDENTIALS,
+        credentials = service_account.Credentials.from_service_account_info(
+            info=GMAIL_CREDENTIALS,
             subject=GMAIL_FROM_EMAIL,
-            scopes=cls.SCOPES,
+            scopes=["https://mail.google.com/"],
         )
-        return build("gmail", "v1", credentials=credentials)
+        return build(serviceName="gmail", version="v1", credentials=credentials)
 
     @classmethod
-    def get_message(cls, history_id: str) -> dict | None:
+    def subscribe(cls) -> dict:
+        """Subscribe a PubSub topic to a Gmail label."""
+        service = cls._authenticate()
+        logger.info(f"Subscribing to Gmail label {GMAIL_LABEL} with topic {GMAIL_TOPIC}")
+
+        request = {
+            "labelFilterBehavior": "INCLUDE",
+            "labelIds": [GMAIL_LABEL],
+            "topicName": GMAIL_TOPIC,
+        }
+        response = service.users().watch(userId=GMAIL_USER_ID, body=request).execute()
+        logger.info(f"Subscribed successfully. Expiration: {response.get('expiration')}")
+        return response
+
+    @classmethod
+    def get_message(cls) -> Message | None:
         """
-        Retrieve the message associated with a specific message history ID.
+        Retrieve the message associated with a specific label.
+
+        Returns:
+            The message data as a dictionary, or None if not found
+
+        """
+        service = cls._authenticate()
+        response = service.users().messages().list(userId=GMAIL_USER_ID, labelIds=[GMAIL_LABEL]).execute()
+
+        if messages := response.get("messages"):
+            message = service.users().messages().get(userId=GMAIL_USER_ID, id=messages[0]["id"]).execute()
+            return Message(message)
+
+        logger.warning(f"No messages found in Gmail for label {GMAIL_LABEL}")
+        return None
+
+    @classmethod
+    def get_message_from_history(cls, history_id: str) -> Message | None:
+        """
+        Retrieve the last message associated with a specific label starting from a specific history ID.
 
         Args:
             history_id: The message history ID to look up
@@ -47,13 +104,17 @@ class Gmail:
 
         """
         service = cls._authenticate()
-        response = service.users().history().list(userId=GMAIL_USER_ID, startHistoryId=history_id).execute()
+        history = service.users().history()
+        response = history.list(userId=GMAIL_USER_ID, startHistoryId=history_id, labelId=GMAIL_LABEL).execute()
 
-        for history in response.get("history", []):
+        # NOTE: The history is reversed to get the last message
+        for history in reversed(response.get("history", [])):
             for message_added in history.get("messagesAdded", []):
                 if message_id := message_added.get("message", {}).get("id"):
-                    return service.users().messages().get(userId=GMAIL_USER_ID, id=message_id).execute()
+                    message = service.users().messages().get(userId=GMAIL_USER_ID, id=message_id).execute()
+                    return Message(message)
 
+        logger.warning(f"No messages found in Gmail for label {GMAIL_LABEL} starting from history ID {history_id}")
         return None
 
     @classmethod
@@ -89,3 +150,28 @@ class Gmail:
         service = cls._authenticate()
 
         return service.users().messages().send(userId=GMAIL_USER_ID, body={"raw": raw_message}).execute()
+
+    @classmethod
+    def get_last_message_from(cls, sender: str) -> Message | None:
+        """
+        Retrieve the last message from a specific sender.
+
+        Returns:
+            The message data as a dictionary, or None if not found
+
+        """
+        service = cls._authenticate()
+        response = service.users().messages().list(userId=GMAIL_USER_ID, labelIds=[GMAIL_LABEL]).execute()
+
+        if not (messages := response.get("messages")):
+            logger.warning(f"No messages found in Gmail for label {GMAIL_LABEL}")
+            return None
+
+        for message_id in map(itemgetter("id"), messages):
+            message = service.users().messages().get(userId=GMAIL_USER_ID, id=message_id).execute()
+            real_sender = next(header["value"] for header in message["payload"]["headers"] if header["name"] == "From")
+            if sender in real_sender:
+                return Message(message)
+
+        logger.warning(f"No messages found in Gmail for sender {sender}")
+        return None
